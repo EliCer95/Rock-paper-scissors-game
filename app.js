6398720b1344b5db39a8d182f755b17870dccdef
EliCer95 <89043035+EliCer95@users.noreply.github.com>
2023-11-22 14:35:51 +0000

    Moved the player move into a function

--- app.js
+++ app.js
@@ -1,3 +1,55 @@
+function playGame(playerMove) {
+  const computerMove = pickComputerMove();
+
+  let result = "";
+
+  if (playerMove === "scissors") {
+    if (computerMove === "rock") {
+      result = "You lose!";
+    } else if (computerMove === "paper") {
+      result = "You win!";
+    } else if (computerMove === "scissors") {
+      result = "It's a tie!";
+    }
+
+    alert(
+      `You picked ${playerMove}, the computer picked ${computerMove}, ${result}`
+    );
+  } else if (playerMove === "paper") {
+    const computerMove = pickComputerMove();
+
+    let result = " ";
+
+    if (computerMove === "rock") {
+      result = "You win!";
+    } else if (computerMove === "paper") {
+      result = "It's a tie!";
+    } else if (computerMove === "scissors") {
+      result = "You lose!";
+    }
+
+    alert(
+      `You picked ${playerMove}, the computer picked ${computerMove}, ${result}`
+    );
+  } else if (playerMove === "rock") {
+    const computerMove = pickComputerMove();
+
+    let result = "";
+
+    if (computerMove === "rock") {
+      result = "It's a tie!";
+    } else if (computerMove === "paper") {
+      result = "You lose!";
+    } else if (computerMove === "scissors") {
+      result = "You win!";
+    }
+
+    alert(
+      `You picked ${playerMove}, the computer picked ${computerMove}, ${result}`
+    );
+  }
+}
+
 function pickComputerMove() {
   const randomNumber = Math.random();
   let computerMove = " ";

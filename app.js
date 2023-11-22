f7f97cbcd3246c50512424c07f31a5e4e8bfa7ee
EliCer95 <89043035+EliCer95@users.noreply.github.com>
2023-11-22 11:59:01 +0000

    Moved the computerMove inside a function

---
+++ app.js
@@ -0,0 +1,15 @@
+function pickComputerMove() {
+  const randomNumber = Math.random();
+
+  if (randomNumber >= 0 && randomNumber < 1 / 3) {
+    computerMove = "rock";
+  } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
+    computerMove = "paper";
+  } else if (randomNumber >= 2 / 3) {
+    computerMove = "scissors";
+  }
+
+  return;
+}
+
+let computerMove = " ";
